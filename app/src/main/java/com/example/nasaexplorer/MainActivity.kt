7c39c9ad9d1c704d04e780eff252774aa16f0787
Chris Pangalos <chris.pangalos@metaswitch.com>
2025-03-04 15:30:14 +0000

    Initial Commit.

---
+++ app/src/main/java/com/example/nasaexplorer/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.nasaexplorer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.nasaexplorer.ui.theme.NasaExplorerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NasaExplorerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Row(modifier.padding(all = 8.dp), horizontalArrangement = Arrangement.Center) {
+        Image(
+            painter = painterResource(R.drawable.nasa_meatball),
+            contentDescription = "Nasa Round Logo",
+            modifier = Modifier.size(200.dp)
+        )
+    }
+    Spacer(modifier= Modifier.width(20.dp))
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NasaExplorerTheme {
+        Greeting("Android")
+    }
+}
